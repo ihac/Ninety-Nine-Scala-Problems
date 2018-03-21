1f51a44203f369c353e1a913d2aaef771c7b59a2
Xiao An <hac@zju.edu.cn>
2018-03-21 15:26:42 +0800

    refactor P06
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/main/scala/P06/P06.scala
@@ -0,0 +1,6 @@
+package P06
+
+object P06 {
+  def isPalindrome[T](l: List[T]): Boolean = l == l.reverse
+  // maybe we could use reverse() from package P05
+}
---
+++ src/test/scala/validation/P06TestSuite.scala
@@ -0,0 +1,27 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P06._
+
+@RunWith(classOf[JUnitRunner])
+class P06TestSuite extends FunSuite {
+  test("palindrome check - ordinary Int list: isPalindrome(List(1, 2, 3, 4)) = false") {
+    assert(P06.isPalindrome(List(1, 2, 3, 4)) === false)
+  }
+  test("palindrome check - ordinary Int list: isPalindrome(List(1, 2, 2, 1)) = true") {
+    assert(P06.isPalindrome(List(1, 2, 2, 1)) === true)
+  }
+  test("palindrome check - ordinary String list: isPalindrome(List(\"hello\", \"world\", \"hello\")) = true") {
+    assert(P06.isPalindrome(List("hello", "world", "hello")) === true)
+  }
+  test("palindrome check - one-item list: isPalindrome(List(3)) = true") {
+    assert(P06.isPalindrome(List(3)) === true)
+  }
+  test("palindrome check - empty list: isPalindrome(List()) = true") {
+    assert(P06.isPalindrome(List()) === true)
+  }
+}
+
