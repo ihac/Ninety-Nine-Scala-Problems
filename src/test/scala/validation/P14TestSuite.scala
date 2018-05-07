611680241c4b93055d3b57f0932659d7e145ebd7
Xiao An <hac@zju.edu.cn>
2018-05-07 19:42:52 +0800

    refactor P14
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/test/scala/validation/P14TestSuite.scala
@@ -0,0 +1,23 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P14._
+
+@RunWith(classOf[JUnitRunner])
+class P14TestSuite extends FunSuite {
+  test("[uplicate an ordinary list: duplicate(List('a, 'b, 'c, 'c, 'd)) = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)") {
+    assert(P14.duplicate(List('a, 'b, 'c, 'c, 'd)) === List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+  test("[uplicate an ordinary list: duplicate(List(1, 3, 2, 8)) = List(1, 3, 2, 8)") {
+    assert(P14.duplicate(List(1, 3, 2, 8)) === List(1, 1, 3, 3, 2, 2, 8, 8))
+  }
+  test("duplicate a one-item list: duplicate(List(3)) = List(3, 3)") {
+    assert(P14.duplicate(List(3)) === List(3, 3))
+  }
+  test("duplicate an empty list: duplicate(List()) = List()") {
+    assert(P14.duplicate(List()) === List())
+  }
+}
