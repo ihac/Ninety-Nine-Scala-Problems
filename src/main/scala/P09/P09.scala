f4d20d99230d332631e4e7e772ba0eb1e79414f4
Xiao An <hac@zju.edu.cn>
2018-05-07 16:12:01 +0800

    refactor P09
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/main/scala/P09/P09.scala
@@ -0,0 +1,20 @@
+package P09
+
+object P09 {
+  def pack[T](l: List[T]): List[List[T]] = l.foldRight(List[List[T]]()) {
+    case (e, h::ls) if e == h.head => (e::h)::ls
+    case (e, ll) => List(e)::ll
+  }
+
+  def pack_TailRecursion[T](l: List[T]): List[List[T]] = {
+    def _pack(prev: List[List[T]], next: List[T]): List[List[T]] = next match {
+      case Nil => prev.reverse
+      // more effective using takeWhile + dropWhile
+      case h::ls => _pack(next.takeWhile(_ == h)::prev, next.dropWhile(_ == h))
+//      case h::ls =>
+//        if (!prev.isEmpty && prev.head.head == h) _pack((h::prev.head)::prev.tail, ls)
+//        else _pack(List(h)::prev, ls)
+    }
+    _pack(Nil, l)
+  }
+}
---
+++ src/test/scala/validation/P09TestSuite.scala
@@ -0,0 +1,37 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P09._
+
+@RunWith(classOf[JUnitRunner])
+class P09TestSuite extends FunSuite {
+  test("pack an ordinary list: pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))") {
+    assert(P09.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }
+  test("pack an ordinary list: pack(List(1, 1, 2, 1, 3, 5, 8)) = List(1, 2, 1, 3, 5, 8)") {
+    assert(P09.pack(List(1, 1, 2, 1, 3, 5, 8)) === List(List(1, 1), List(2), List(1), List(3), List(5), List(8)))
+  }
+  test("pack an one-item list: pack(List(3)) = List(3)") {
+    assert(P09.pack(List(3)) === List(List(3)))
+  }
+  test("pack an empty list: pack(List()) = List()") {
+    assert(P09.pack(List()) === List())
+  }
+  test("[Tail Recursion] pack an ordinary list: pack_TailRecursion(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))") {
+    assert(P09.pack_TailRecursion(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }
+  test("[Tail Recursion] pack an ordinary list: pack_TailRecursion(List(1, 1, 2, 1, 3, 5, 8)) = List(1, 2, 1, 3, 5, 8)") {
+    assert(P09.pack_TailRecursion(List(1, 1, 2, 1, 3, 5, 8)) === List(List(1, 1), List(2), List(1), List(3), List(5), List(8)))
+  }
+  test("[Tail Recursion] pack an one-item list: pack_TailRecursion(List(3)) = List(3)") {
+    assert(P09.pack_TailRecursion(List(3)) === List(List(3)))
+  }
+  test("[Tail Recursion] pack an empty list: pack_TailRecursion(List()) = List()") {
+    assert(P09.pack_TailRecursion(List()) === List())
+  }
+}
+
+
