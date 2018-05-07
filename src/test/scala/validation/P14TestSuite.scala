ba964fa5831b21ba898dc41870b674aaf7f5e3e4
Xiao An <hac@zju.edu.cn>
2018-05-07 20:13:49 +0800

    update P14 with a few changes
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- src/test/scala/validation/P14TestSuite.scala
+++ src/test/scala/validation/P14TestSuite.scala
@@ -8,10 +8,10 @@ import P14._
 
 @RunWith(classOf[JUnitRunner])
 class P14TestSuite extends FunSuite {
-  test("[uplicate an ordinary list: duplicate(List('a, 'b, 'c, 'c, 'd)) = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)") {
+  test("duplicate an ordinary list: duplicate(List('a, 'b, 'c, 'c, 'd)) = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)") {
     assert(P14.duplicate(List('a, 'b, 'c, 'c, 'd)) === List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
   }
-  test("[uplicate an ordinary list: duplicate(List(1, 3, 2, 8)) = List(1, 3, 2, 8)") {
+  test("duplicate an ordinary list: duplicate(List(1, 3, 2, 8)) = List(1, 3, 2, 8)") {
     assert(P14.duplicate(List(1, 3, 2, 8)) === List(1, 1, 3, 3, 2, 2, 8, 8))
   }
   test("duplicate a one-item list: duplicate(List(3)) = List(3, 3)") {
