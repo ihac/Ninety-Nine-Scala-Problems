30ecc8df71a370525cfe2a0ca65434444fc3c5f2
Xiao An <hac@zju.edu.cn>
2018-05-07 16:34:32 +0800

    update P10 with a few changes
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- src/test/scala/validation/P10TestSuite.scala
+++ src/test/scala/validation/P10TestSuite.scala
@@ -8,13 +8,13 @@ import P10._
 
 @RunWith(classOf[JUnitRunner])
 class P10TestSuite extends FunSuite {
-  test("encode an ordinary list: encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))") {
+  test("encode an ordinary list: encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))") {
     assert(P10.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
   }
-  test("encode an ordinary list: encode(List(1, 1, 2, 1, 3, 5, 8)) = List(1, 2, 1, 3, 5, 8)") {
+  test("encode an ordinary list: encode(List(1, 1, 2, 1, 3, 5, 8)) = List((2, 1), (1, 2), (1, 1), (1, 3), (1, 5), (1, 8)") {
     assert(P10.encode(List(1, 1, 2, 1, 3, 5, 8)) === List((2, 1), (1, 2), (1, 1), (1, 3), (1, 5), (1, 8)))
   }
-  test("encode an one-item list: encode(List(3)) = List(3)") {
+  test("encode an one-item list: encode(List(3)) = List((1, 3))") {
     assert(P10.encode(List(3)) === List((1, 3)))
   }
   test("encode an empty list: encode(List()) = List()") {
