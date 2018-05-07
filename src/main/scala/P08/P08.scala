5b2801760d195deef91ae1d4a9f7ef73d73039cf
Xiao An <hac@zju.edu.cn>
2018-05-07 15:53:29 +0800

    refactor P08
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/main/scala/P08/P08.scala
@@ -0,0 +1,22 @@
+package P08
+
+object P08 {
+  def compress[T](l: List[T]): List[T] = l.foldRight(List[T]()) {
+    (e, ll) => ll match {
+      case head::_ if head == e => ll
+      case _ => e::ll
+    }
+  }
+
+  // use prepend rather than append when deal with List concatenating
+  def compress_TailRecursion[T](l: List[T]): List[T] = {
+    def _compress[T](orig: List[T], newl: List[T]): List[T] = (orig, newl) match {
+      case (Nil, _) => newl.reverse
+        // more effective using dropWhile
+      case (oh::ol, _) => _compress(orig.dropWhile(_ == oh), oh::newl)
+//      case (oh::ol, nh::_) if (oh == nh) => _compress(ol, newl)
+//      case (oh::ol, _) => _compress(ol, oh::newl)
+    }
+    _compress(l, Nil)
+  }
+}
---
+++ src/test/scala/validation/P08TestSuite.scala
@@ -0,0 +1,37 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P08._
+
+@RunWith(classOf[JUnitRunner])
+class P08TestSuite extends FunSuite {
+  test("compress an ordinary list: compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List('a, 'b, 'c, 'a, 'd, 'e)") {
+    assert(P08.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+  test("compress an ordinary list: compress(List(1, 1, 2, 1, 3, 5, 8)) = List(1, 2, 1, 3, 5, 8)") {
+    assert(P08.compress(List(1, 1, 2, 1, 3, 5, 8)) === List(1, 2, 1, 3, 5, 8))
+  }
+  test("compress an one-item list: commpress(List(3)) = List(3)") {
+    assert(P08.compress(List(3)) === List(3))
+  }
+  test("compress an empty list: compress(List()) = List()") {
+    assert(P08.compress(List()) === List())
+  }
+  test("[Tail Recursion] compress an ordinary list: compress_TailRecursion(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List('a, 'b, 'c, 'a, 'd, 'e)") {
+    assert(P08.compress_TailRecursion(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+  test("[Tail Recursion] compress an ordinary list: compress_TailRecursion(List(1, 1, 2, 1, 3, 5, 8)) = List(1, 2, 1, 3, 5, 8)") {
+    assert(P08.compress_TailRecursion(List(1, 1, 2, 1, 3, 5, 8)) === List(1, 2, 1, 3, 5, 8))
+  }
+  test("[Tail Recursion] compress an one-item list: commpress_TailRecursion(List(3)) = List(3)") {
+    assert(P08.compress_TailRecursion(List(3)) === List(3))
+  }
+  test("[Tail Recursion] compress an empty list: compress_TailRecursion(List()) = List()") {
+    assert(P08.compress_TailRecursion(List()) === List())
+  }
+}
+
+
