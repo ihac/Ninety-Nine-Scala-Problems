c577537cc3a46e5024a184edb79af020e3b3d68c
Xiao An <hac@zju.edu.cn>
2018-05-07 19:16:52 +0800

    refactor P13
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/main/scala/P13/P13.scala
@@ -0,0 +1,23 @@
+package P13
+
+object P13 {
+  def encodeDirect[T](l: List[T]): List[(Int, T)] = {
+    def _encodeDirect[T](prev: List[(Int, T)], next: List[T]): List[(Int, T)] = next match {
+      case Nil => prev.reverse
+      // more elegant using prefixLength + drop instead of takeWhile + dropWhile
+      case h::tail => {
+        val len = next.prefixLength(_ == h)
+        _encodeDirect((len, h)::prev, next.drop(len))
+      }
+//      case h::tail => _encodeDirect((next.takeWhile(_ == h).length, h)::prev, next.dropWhile(_ == h))
+    }
+    _encodeDirect(Nil, l)
+  }
+
+  def encodeDirect_FoldRight[T](l: List[T]): List[(Int, T)] = l.foldRight(List[(Int, T)]()) {
+    (e, ll) => ll match {
+      case pair::tail if pair._2 == e => (pair._1 + 1, pair._2)::tail
+      case _ => (1, e)::ll
+    }
+  }
+}
---
+++ src/test/scala/validation/P13TestSuite.scala
@@ -0,0 +1,35 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P13._
+
+@RunWith(classOf[JUnitRunner])
+class P13TestSuite extends FunSuite {
+  test("encodeDirect an ordinary list: encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))") {
+    assert(P13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+  }
+  test("encodeDirect an ordinary list: encodeDirect(List(1, 1, 2, 1, 3, 5, 8)) = List((2, 1), (1, 2), (1, 1), (1, 3), (1, 5), (1, 8)") {
+    assert(P13.encodeDirect(List(1, 1, 2, 1, 3, 5, 8)) === List((2, 1), (1, 2), (1, 1), (1, 3), (1, 5), (1, 8)))
+  }
+  test("encodeDirect an one-item list: encodeDirect(List(3)) = List((1, 3))") {
+    assert(P13.encodeDirect(List(3)) === List((1, 3)))
+  }
+  test("encodeDirect an empty list: encodeDirect(List()) = List()") {
+    assert(P13.encodeDirect(List()) === List())
+  }
+  test("[Fold Right] encodeDirect an ordinary list: encodeDirect_FoldRight(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))") {
+    assert(P13.encodeDirect_FoldRight(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+  }
+  test("[Fold Right] encodeDirect an ordinary list: encodeDirect_FoldRight(List(1, 1, 2, 1, 3, 5, 8)) = List((2, 1), (1, 2), (1, 1), (1, 3), (1, 5), (1, 8)") {
+    assert(P13.encodeDirect_FoldRight(List(1, 1, 2, 1, 3, 5, 8)) === List((2, 1), (1, 2), (1, 1), (1, 3), (1, 5), (1, 8)))
+  }
+  test("[Fold Right] encodeDirect an one-item list: encodeDirect_FoldRight(List(3)) = List((1, 3))") {
+    assert(P13.encodeDirect_FoldRight(List(3)) === List((1, 3)))
+  }
+  test("[Fold Right] encodeDirect an empty list: encodeDirect_FoldRight(List()) = List()") {
+    assert(P13.encodeDirect_FoldRight(List()) === List())
+  }
+}
