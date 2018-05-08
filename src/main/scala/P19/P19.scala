d8c21142f24385a30b0fb9294e8e9c18d057e372
Xiao An <hac@zju.edu.cn>
2018-05-08 15:25:31 +0800

    refactor P19
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/main/scala/P19/P19.scala
@@ -0,0 +1,9 @@
+package P19
+
+object P19 {
+  def rotate[T](n: Int, l: List[T]): List[T] = {
+    val nn = if (l.isEmpty) 0 else n % l.length
+    if (nn >= 0) l.drop(nn):::l.take(nn)
+    else rotate(nn + l.length, l)
+  }
+}
---
+++ src/test/scala/validation/P19TestSuite.scala
@@ -0,0 +1,32 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P19._
+
+@RunWith(classOf[JUnitRunner])
+class P19TestSuite extends FunSuite {
+  test("rotate an ordinary list: rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) = List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)") {
+    assert(P19.rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c))
+  }
+  test("rotate an ordinary list: rotate(2, List(1, 3, 2, 8)) = List(2, 8, 1, 3)") {
+    assert(P19.rotate(2, List(1, 3, 2, 8)) === List(2, 8, 1, 3))
+  }
+  test("rotate an ordinary list by 0: rotate(0, List(1, 2, 3, 4)) = List(1, 2, 3, 4)") {
+    assert(P19.rotate(0, List(1, 2, 3, 4)) === List(1, 2, 3, 4))
+  }
+  test("rotate an ordinary list by a negative number: rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)") {
+    assert(P19.rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) === List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i))
+  }
+  test("rotate an ordinary list by a large number: rotate(7, List(1, 2, 3, 4)) = List(4, 1, 2, 3)") {
+    assert(P19.rotate(7, List(1, 2, 3, 4)) === List(4, 1, 2, 3))
+  }
+  test("rotate an ordinary list by a large number: rotate(-7, List(1, 2, 3, 4)) = List(2, 3, 4, 1)") {
+    assert(P19.rotate(-7, List(1, 2, 3, 4)) === List(2, 3, 4, 1))
+  }
+  test("rotate an empty list: rotate(3, List()) = List()") {
+    assert(P19.rotate(3, List()) === List())
+  }
+}
