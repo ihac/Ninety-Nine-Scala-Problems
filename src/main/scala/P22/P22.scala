8a422e8add5eef8e45333afe24a4a17b01b78583
Xiao An <hac@zju.edu.cn>
2018-05-09 10:34:13 +0800

    solve P22
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/main/scala/P22/P22.scala
@@ -0,0 +1,12 @@
+package P22
+
+object P22 {
+  def range(i: Int, j: Int): List[Int] = (i to j).toList
+
+  def range_TailRecursion(i: Int, j: Int): List[Int] = {
+    def _range(k: Int, prev: List[Int]): List[Int] =
+      if (k > j) prev.reverse
+      else _range(k+1, k::prev)
+    _range(i, Nil)
+  }
+}
---
+++ src/test/scala/validation/P22TestSuite.scala
@@ -0,0 +1,42 @@
+package validation
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import P22._
+
+@RunWith(classOf[JUnitRunner])
+class P22TestSuite extends FunSuite {
+  test("range between two positive integers: range(4, 9) = List(4, 5, 6, 7, 8, 9)") {
+    assert(P22.range(4, 9) === List(4, 5, 6, 7, 8, 9))
+  }
+  test("range between two negative integers: range(-5, -1) = List(-5, -4, -3, -2, -1)") {
+    assert(P22.range(-5, -1) === List(-5, -4, -3, -2, -1))
+  }
+  test("range between two identical integers: range(2, 2) = List(2)") {
+    assert(P22.range(2, 2) === List(2))
+  }
+  test("range between a negative integer and a positive integer: range(-2, 2) = List(-2, -1, 0, 1, 2)") {
+    assert(P22.range(-2, 2) === List(-2, -1, 0, 1, 2))
+  }
+  test("range between two integers where the former is larger than the latter: range(4, 2) = List()") {
+    assert(P22.range(4, 2) === List())
+  }
+
+  test("[Tail Recursion] range between two positive integers: range_TailRecursion(4, 9) = List(4, 5, 6, 7, 8, 9)") {
+    assert(P22.range_TailRecursion(4, 9) === List(4, 5, 6, 7, 8, 9))
+  }
+  test("[Tail Recursion] range between two negative integers: range_TailRecursion(-5, -1) = List(-5, -4, -3, -2, -1)") {
+    assert(P22.range_TailRecursion(-5, -1) === List(-5, -4, -3, -2, -1))
+  }
+  test("[Tail Recursion] range between two identical integers: range_TailRecursion(2, 2) = List(2)") {
+    assert(P22.range_TailRecursion(2, 2) === List(2))
+  }
+  test("[Tail Recursion] range between a negative integer and a positive integer: range_TailRecursion(-2, 2) = List(-2, -1, 0, 1, 2)") {
+    assert(P22.range_TailRecursion(-2, 2) === List(-2, -1, 0, 1, 2))
+  }
+  test("[Tail Recursion] range between two integers where the former is larger than the latter: range_TailRecursion(4, 2) = List()") {
+    assert(P22.range_TailRecursion(4, 2) === List())
+  }
+}
