537f2dbe1e17e9073207fe4b8d69ff2493f31421
Xiao An <hac@zju.edu.cn>
2018-05-15 14:42:26 +0800

    update P26
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- src/test/scala/validation/P26TestSuite.scala
+++ src/test/scala/validation/P26TestSuite.scala
@@ -18,7 +18,6 @@ class P26TestSuite extends FunSuite {
   }
   test("combinations of 4 distinct objects from an ordinary set: combinations(3, List(2, 3, 1, 5, 6))") {
     val res = P26.combinations(4, List(2, 3, 1, 5, 6))
-    println(res)
     assert(res.length === Combination.compute(5, 4))
     assert(res.forall(
       l => l.length == 4 && l.isSub(List(2, 3, 1, 5, 6))
@@ -26,6 +25,7 @@ class P26TestSuite extends FunSuite {
   }
   test("combinations of 3 distinct objects from a three-item set: combinations(3, List(2, 3, 4))") {
     val res = P26.combinations(3, List(2, 3, 4))
+    println(res)
     assert(res.length === Combination.compute(3, 3))
     assert(res.forall(
       l => l.length == 3 && l.isSub(List(2, 3, 4))
@@ -40,7 +40,7 @@ class P26TestSuite extends FunSuite {
   }
   test("combinations of 0 distinct object from an ordinary set: combinations(0, List(2, 0, 1, 8))") {
     val res = P26.combinations(0, List(2, 0, 1, 8))
-    assert(res === Nil)
+    assert(res === List(Nil))
   }
   test("combinations of 3 distinct objects from a two-item set: combinations(3, List(2, 3))") {
     intercept[IllegalArgumentException] {
@@ -52,4 +52,47 @@ class P26TestSuite extends FunSuite {
       P26.combinations(3, List())
     }
   }
+
+  test("[FP] combinations of 3 distinct objects from an ordinary set: combinations_FP(3, List('a, 'b, 'c, 'd, 'e, 'f))") {
+    val res = P26.combinations_FP(3, List('a, 'b, 'c, 'd, 'e, 'f))
+    assert(res.length === Combination.compute(6, 3))
+    assert(res.forall(
+      l => l.length == 3 && l.isSub(List('a, 'b, 'c, 'd, 'e, 'f))
+    ))
+  }
+  test("[FP] combinations of 4 distinct objects from an ordinary set: combinations_FP(3, List(2, 3, 1, 5, 6))") {
+    val res = P26.combinations_FP(4, List(2, 3, 1, 5, 6))
+    assert(res.length === Combination.compute(5, 4))
+    assert(res.forall(
+      l => l.length == 4 && l.isSub(List(2, 3, 1, 5, 6))
+    ))
+  }
+  test("[FP] combinations of 3 distinct objects from a three-item set: combinations_FP(3, List(2, 3, 4))") {
+    val res = P26.combinations_FP(3, List(2, 3, 4))
+    assert(res.length === Combination.compute(3, 3))
+    assert(res.forall(
+      l => l.length == 3 && l.isSub(List(2, 3, 4))
+    ))
+  }
+  test("[FP] combinations of 1 distinct object from an ordinary set: combinations_FP(1, List(2, 0, 1, 8))") {
+    val res = P26.combinations_FP(1, List(2, 0, 1, 8))
+    assert(res.length === Combination.compute(4, 1))
+    assert(res.forall(
+      l => l.length == 1 && l.isSub(List(2, 0, 1, 8))
+    ))
+  }
+  test("[FP] combinations of 0 distinct object from an ordinary set: combinations_FP(0, List(2, 0, 1, 8))") {
+    val res = P26.combinations_FP(0, List(2, 0, 1, 8))
+    assert(res === List(Nil))
+  }
+//  test("[FP] combinations of 3 distinct objects from a two-item set: combinations_FP(3, List(2, 3))") {
+//    intercept[IllegalArgumentException] {
+//      P26.combinations_FP(3, List(2, 3))
+//    }
+//  }
+//  test("[FP] combinations of 3 distinct objects from an empty set: combinations_FP(3, List())") {
+//    intercept[IllegalArgumentException] {
+//      P26.combinations_FP(3, List())
+//    }
+//  }
 }
