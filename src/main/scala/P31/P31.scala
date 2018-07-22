3648bb176fc905273d79290bbaf638232fa0d735
Xiao An <hac@zju.edu.cn>
2018-07-22 16:33:33 +0800

    update P31 by improving the performance of isPrime
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- src/main/scala/P31/P31.scala
+++ src/main/scala/P31/P31.scala
@@ -3,13 +3,14 @@ package P31
 import scala.language.implicitConversions
 
 class P31(val num: Int) {
-  def isPrime: Boolean = (P31.primeStream dropWhile (_ < num)).head == num
+  def isPrime: Boolean =
+    num >= 2 && (P31.primeStream takeWhile (_ <= Math.sqrt(num)) forall (num % _ != 0))
 }
 
 object P31 {
   implicit def toP31Int(num: Int): P31 = new P31(num)
 
-  def filter(stream: Stream[Int]): Stream[Int] =
-    stream.head #:: filter(stream filter (_  % stream.head != 0))
-  val primeStream = filter(Stream.from(2))
+  def loop(stream: Stream[Int]): Stream[Int] =
+    stream.head #:: loop(stream filter (_ % stream.head != 0))
+  val primeStream: Stream[Int] = loop(Stream.from(2))
 }
