fed6fea55c6967a37373d7d85620676f446801e4
Xiao An <hac@zju.edu.cn>
2018-07-23 17:31:35 +0800

    solve P37
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/test/scala/validation/P37TestSuite.scala
@@ -0,0 +1,25 @@
+package validation
+
+import P37.P37._
+import org.junit.runner.RunWith
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.BooleanOperators
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class P37TestSuite extends FunSuite {
+  test("totient_improve should always be consistent with totient") {
+    import P34.P34._
+    val prop = forAll { n: Int =>
+      (n > 0 && n < 100000) ==> (n.totient == n.totient_improve)
+    }
+    prop.check()
+  }
+
+  /**
+    * There is no need to test totient_improve by specific cases, since we've
+    * already proved that its result is always consistent with totient', which's
+    * proved to be correct in solution 34.
+    */
+}
